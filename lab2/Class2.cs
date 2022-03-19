166dee4e5654c907794357a8f7c2ae66100f0a5b
klaudiusz.kolegowicz <klaudiusz.kolegowicz@PK5-21>
2022-03-19 12:22:12 +0100

    Dodaj pliki projektów.

---
+++ lab2/Class2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab2
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        Person(string name, int age)
+        {
+
+            Name = name;
+            Age = age;
+
+        }
+        public bool Equals(Person obj)
+        {
+            return obj.Name == this.Name && obj.Age == this.Age;
+        }
+
+        override public string ToString()
+        {
+            return this.Name + "(" + this.Age.ToString() + " y.o.)";
+        }
+
+    }
+}
